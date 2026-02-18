aa091218a2eb7b3f4d540bc9668f3ee15cf89ca8
Casey <108437478+casey-shea05@users.noreply.github.com>
2026-02-18 18:40:27 +0000

    feat: write function for computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+// Variables
+const choices = ["rock", "paper", "scissors"];
+let computerChoice = "";
+
+// Functions
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+  // generate a random number for index
+  const index = getRandomInt(3);
+
+  // get rock paper or scissors based on random index
+  computerChoice = choices[index];
+  return computerChoice;
+}
+
+// Main Code
